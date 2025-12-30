692352ae845d2a8c6d17fa642685c9896f85ad10
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-12-30 10:40:27 +0530

    updated app test js file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders home heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/hello, i'm nirajan/i);
+  expect(heading).toBeInTheDocument();
 });
+
